0201090ad702ec8835e48680563f4e9407312d3e
trilochan-behera-dev <trilochan97.dev@gmail.com>
2023-12-26 21:22:08 +0530

    update scrollbar width css

--- client/pages/layout.tsx
+++ client/pages/layout.tsx
@@ -21,12 +21,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                         setSidebarOpen={setSidebarOpen}
                     />
 
-                    <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                    <div className="relative flex flex-1 flex-col overflow-hidden">
                         <Header
                             sidebarOpen={sidebarOpen}
                             setSidebarOpen={setSidebarOpen}
                         />
-                        <main>
+                        <main className="overflow-y-auto overflow-x-hidden scrollbar">
                             <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
                                 {children}
                             </div>
